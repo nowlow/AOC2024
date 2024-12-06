4f6e34fafabdd3d4730c5a035938d73d68678790
Naoufel <nberrada@lithium.paris>
2024-12-06 23:53:23 +0100

    fix: sed syntax for macos

--- new_day.sh
+++ new_day.sh
@@ -6,5 +6,5 @@ cargo new $crate_name
 cp template/main.rs $crate_name/src/main.rs
 cp template/README.md $crate_name/README.md
 
-sed -i "s/{CRATE_NAME}/$crate_name/g" $crate_name/README.md
-sed -i "s/{DAY_NO}/$(echo $day | sed 's/^0*//')/g" $crate_name/README.md
+sed -i '' "s/{CRATE_NAME}/$crate_name/g" $crate_name/README.md
+sed -i '' "s/{DAY_NO}/$(echo $day | sed 's/^0*//')/g" $crate_name/README.md
